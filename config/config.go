79c1f3573129ae0345765db7fe14254543c34a43
Base2106 <39075746+Base2106@users.noreply.github.com>
2019-06-22 17:30:25 +0300

    Update config.go

--- config/config.go
+++ config/config.go
@@ -12,11 +12,11 @@ import (
 
 // Config - параметры запуска бота
 type Config struct {
-	DiscordToken string `json:"ojLKLls6tWr9xNbvPDy0VmGtCAp8QdfP"`
-	VkToken      string `json:"676b69763fcf61146cc625ef41fca4f013d943dd2b6cf677b648df55a2db4518899cb66f9667625c8ef8c"`
-	ChannelID    string `json:"582799966892851200"`
-	GroupID      string `json:"testdiscord2"`
-	LogPath      string `json:"log_path"`
+	DiscordToken string `json:ojLKLls6tWr9xNbvPDy0VmGtCAp8QdfP`
+	VkToken      string `json:676b69763fcf61146cc625ef41fca4f013d943dd2b6cf677b648df55a2db4518899cb66f9667625c8ef8c`
+	ChannelID    string `json:582799966892851200`
+	GroupID      string `json:testdiscord2`
+	LogPath      string `json:log_path`
 }
 
 // Путь к файлу конфигурации
